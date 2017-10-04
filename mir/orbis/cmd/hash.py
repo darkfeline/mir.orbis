8de809402dca27e3f2ad8e891ab528756f9f490a
Allen Li <darkfeline@felesatra.moe>
2017-10-04 00:01:48 -0700

    Fix bug

--- mir/orbis/cmd/hash.py
+++ mir/orbis/cmd/hash.py
@@ -33,7 +33,7 @@ def main(args):
 
     hashdir = pictus.find_hashdir(args.files[0])
     logger.info('Found hash dir %s', hashdir)
-    pictus.add_files(hashdir, args.files)
+    pictus.add_all(hashdir, args.files)
     return 0
 
 
