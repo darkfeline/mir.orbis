b7e57c2145ffe875afcca2ef95fbc93065d5ce3f
Allen Li <darkfeline@felesatra.moe>
2017-12-12 22:27:23 -0800

    Remove unused imports

--- tests/test_pictus.py
+++ tests/test_pictus.py
@@ -12,10 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import io
 import os
 from pathlib import Path
-from pathlib import PurePath
 
 import pytest
 
