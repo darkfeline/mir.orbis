c8c68244c17ffc976b4b16b739426637ac055e29
Allen Li <darkfeline@felesatra.moe>
2017-12-16 15:31:58 -0800

    Fix docstring

--- mir/orbis/hashcache.py
+++ mir/orbis/hashcache.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Working with hashed archives."""
+"""Hash cache."""
 
 import os
 from pathlib import Path
